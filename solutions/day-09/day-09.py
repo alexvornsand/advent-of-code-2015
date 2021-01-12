1c2e63b5431f188599f3bfa5c22dff1657438ae4
alexvornsand <ajvornsand@ucdavis.edu>
2021-01-11 17:10:06 -0800

    Create day-09.py

---
+++ solutions/day-09/day-09.py
@@ -0,0 +1,35 @@
+# advent of code 2015
+# day 9
+
+# part 1
+import itertools
+
+input = open('day-09.txt', 'r').read()
+
+def getTravelDistance(input, partTwo = False):
+    distances = {}
+    for i in input.split('\n'):
+        fromCity = i.split(' ')[0]
+        toCity = i.split(' ')[2]
+        distance = int(i.split(' ')[4])
+        if fromCity in distances.keys():
+            distances[fromCity][toCity] = distance
+        else:
+            distances[fromCity] = {toCity: distance}
+        if toCity in distances.keys():
+            distances[toCity][fromCity] = distance
+        else:
+            distances[toCity] = {fromCity: distance}
+    orders = list(itertools.permutations(list(set([item for sublist in [[i.split(' ')[0], i.split(' ')[2]] for i in input.split('\n')] for item in sublist]))))
+    def travelDistance(itin):
+        return(sum([distances[itin[i]][itin[i + 1]] for i in range(7)]))
+    travelDistances = [travelDistance(i) for i in orders]
+    if partTwo == False:
+        return(min(travelDistances))
+    else:
+        return(max(travelDistances))
+
+print(getTravelDistance(input))
+
+# part 2
+print(getTravelDistance(input, partTwo = True))
