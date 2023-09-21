e9f9cb66088b1669becdc720491bbf551485008e
alexvornsand <ajvornsand@ucdavis.edu>
2023-09-21 15:10:46 -0500

    Update day-02.py

--- solutions/day-02/day-02.py
+++ solutions/day-02/day-02.py
@@ -1,24 +1,30 @@
 # advent of code 2015
 # day 2
 
-# part 1
-input = open('day-02.txt', 'r').read()
+file = 'input.txt'
 
-def orderPaper(input, partTwo = False):
-    boxes = input.split('\n')
-    def processBox(box, partTwo):
-        dims = [int(x) for x in box.split('x')]
-        if partTwo == False:
-            sides = [dims[0] * dims[1], dims[0] * dims[2], dims[1] * dims[2]]
-            sqFeet = 2 * sum(sides) + min(sides)
-        else:
-            circ = 2 * sum(sorted(dims)[0:2])
-            vol = dims[0] * dims[1] * dims[2]
-            sqFeet = circ + vol
-        return(sqFeet)
-    return(sum([processBox(x, partTwo = partTwo) for x in boxes]))
+class Present:
+    def __init__(self, input):
+        self.width, self.length, self.height = (int(x) for x in input.split('x'))
+        self.bottom = self.width*self.length
+        self.front = self.width*self.height
+        self.side = self.length*self.height
+        self.smallest_surface = min(self.bottom, self.front, self.side)
+        self.surface_area = 2*self.bottom + 2*self.front + 2*self.side
+        self.paper_required = self.surface_area + self.smallest_surface
+        self.ribbon_circ = 2*sum(sorted([self.width, self.length, self.height])[:2])
+        self.ribbon_bow = self.width*self.length*self.height
+        self.ribbon_required = self.ribbon_circ + self.ribbon_bow
 
-orderPaper(input)
+def part_1(file):
+    print('Part 1:', sum([Present(x).paper_required for x in open(file, 'r').read().rstrip().split('\n')]), sep='\n')
 
-# part 2
-orderPaper(input, partTwo = True)
+def part_2(file):
+    print('Part 2:', sum([Present(x).ribbon_required for x in open(file, 'r').read().rstrip().split('\n')]), sep='\n')
+
+def main():
+    part_1(file)
+    part_2(file)
+
+if __name__ == '__main__':
+    main()
