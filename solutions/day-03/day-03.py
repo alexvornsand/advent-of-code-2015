d2d295e73faf0c1496c2c617d90b88131936f480
alexvornsand <ajvornsand@ucdavis.edu>
2023-09-22 17:00:19 -0500

    Update day-03.py

--- solutions/day-03/day-03.py
+++ solutions/day-03/day-03.py
@@ -1,43 +1,70 @@
 # advent of code 2015
 # day 3
 
-# part 1
-input = open('day-03.txt', 'r').read()
+file = 'input.txt'
 
-def countHouses(input, partTwo = False):
-    x = 0
-    y = 0
-    X = 0
-    Y = 0
-    i = 0
-    addresses = {str(x) + ',' + str(y): 1}
-    for sym in [symb for symb in input]:
-        if partTwo == True & i % 2 != 0:
-            if sym == '>':
-                X += 1
-            elif sym == '<':
-                X -= 1
-            elif sym == '^':
-                Y += 1
-            else:
-                Y -= 1
-            key = str(X) + ',' + str(Y)
+class Path:
+    def __init__(self):
+        self.santa_cursor = [0,0]
+        self.robot_cursor = [0,0]
+        self.locations_visited = set([(0,0)])
+        self.children_visited = len(self.locations_visited)
+        
+    def santa_move(self, movement):
+        if movement == '^':
+            shift = [0,1]
+        elif movement == '>':
+            shift = [1,0]
+        elif movement == 'v':
+            shift = [0,-1]
+        elif movement == '<':
+            shift = [-1,0]
         else:
-            if sym == '>':
-                x += 1
-            elif sym == '<':
-                x -= 1
-            elif sym == '^':
-                y += 1
-            else:
-                y -= 1
-            key = str(x) + ',' + str(y)
-        if(key not in addresses.keys()):
-            addresses[key] = 1
-        i += 1
-    return(len(addresses.keys()))
+            shift = [0,0]
+        self.santa_cursor = [x + y for x,y in zip(self.santa_cursor, shift)]
+        self.locations_visited.add(tuple(self.santa_cursor))
+        self.children_visited = len(self.locations_visited)
 
-countHouses(input)
+    
+    def robot_move(self, movement):
+        if movement == '^':
+            shift = [0,1]
+        elif movement == '>':
+            shift = [1,0]
+        elif movement == 'v':
+            shift = [0,-1]
+        elif movement == '<':
+            shift = [-1,0]
+        else:
+            shift = [0,0]
+        self.robot_cursor = [x + y for x,y in zip(self.robot_cursor, shift)]
+        self.locations_visited.add(tuple(self.robot_cursor))
+        self.children_visited = len(self.locations_visited)
 
-# part 2
-countHouses(input, partTwo = True)
+def part_1(directions):
+    path = Path()
+    print(path.locations_visited)
+    for x in directions:
+        path.santa_move(x)
+    print('Part 1:', path.children_visited, sep='\n')
+
+def part_2(directions):
+    path = Path()
+    print(path.locations_visited)
+    i = 0
+    for x in directions:
+        if i % 2 == 0:
+            path.santa_move(x)
+            i += 1
+        else:
+            path.robot_move(x)
+            i += 1
+    print('Part 2:', path.children_visited, sep='\n')
+
+def main():
+    directions = [d for d in open(file, 'r').read().rstrip()]
+    part_1(directions)
+    part_2(directions)
+
+if __name__ == '__main__':
+    main()
